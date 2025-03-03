fd740ee5282b794fa853322757434addd6d7445c
godzilla216 <gojirapizza@gmail.com>
2025-03-03 11:26:20 -0600

    Update script.js

--- pack/script.js
+++ pack/script.js
@@ -25,7 +25,28 @@ const multiplier = 1.05; // Exponential growth rate per OVR
 let cardPendingAction = false; // Tracks if there's a card awaiting action
 
 // Card data
-const cards = ['/point.js'];
+const cards = const cards = [
+    {
+        file: "87_ed_reed_gold.png",
+        typeVideo: "Assets/gold.mp4",
+    },
+    {
+        file: "84_Boogie_Basham_genki_force.png",
+        typeVideo: "Assets/gold.mp4",
+    },
+    {
+        file: "94_Harold_landryIII_rare.png",
+        typeVideo: "Assets/gold.mp4",
+    },
+    {
+        file: "99_Patrick_Manomes_Rare.png",
+        typeVideo: "Assets/gold.mp4",
+    },
+     {
+        file: "97_Josh_dobbs_rare.png",
+        typeVideo: "Assets/gold.mp4",
+    },
+];
     // Replace hyphens with underscores for consistency
     const fileName = card.file.replace(/-/g, "_").replace(".png", "");
     const [overall, firstName, lastName, type] = fileName.split("_");
