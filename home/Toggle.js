01c5181282ce1b003a924a6e9aa84ff82da1ba4c
godzilla216 <gojirapizza@gmail.com>
2025-04-25 21:01:54 -0500

    Delete home/Toggle.js

--- home/Toggle.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('dark-mode-switch').addEventListener('change', function() {
-    if (this.checked) {
-        document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.remove('dark-mode');
-    }
-});
